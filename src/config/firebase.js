a17d45f21c7ff27daab70c1477683301ec7ec4cb
aloksingh321 <42268784+aloksingh321@users.noreply.github.com>
2020-06-30 08:22:16 -0400

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey:'AIzaSyCaqGLmItH3LOQgJ58Ia-rIX7g7FnPcViA',
-  authDomain: 'react-native-ce4d2.web.app',
+  apiKey:'xxxxxxxxxxxxxxxxx',
+  authDomain: 'xxxxxxx',
  
-  projectId: 'react-native-ce4d2',
+  projectId: 'xxxx',
   
-  messagingSenderId:'424048184536',
-  appId: '1:424048184536:android:25abeb8161cb8ac368dbad'
+  messagingSenderId:'xxxxxxxx',
+  appId: 'xxxxxxxxxxx'
 }
 
 // Initialize Firebase
